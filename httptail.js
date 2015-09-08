78e12eca6a6af30e6262f517cad3f4e44b92f83b
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2015-09-08 16:22:22 +0200

    modularized and parallelized - can tail multiple urls

--- httptail.js
+++ httptail.js
@@ -1,84 +1,34 @@
 #!/usr/bin/env node
 
-var request = require('request');
-var fs = require('fs');
-var byline = require('byline');
 var program = require('commander');
-var n = 0;
-var t = true;
-var p = 1000;
-
-var options = {
-    //url: 'http://logs.fe.nord.coll:8003/access_log.csv',
-    //url: 'http://logs.fe.nord.coll:8003/error_log',
-    proxy: false
-};
+var HttpTailer = require('./lib/HttpTailer.js').HttpTailer;
 
 program
-    .arguments('url', 'Url to tail')
-    .usage('url [options]')
-    .version('0.0.1')
+    .version('0.0.3')
+    .arguments('url [otherUrls...]', 'Url to tail')
     .option("-n, --number [lines]", "Number of lines (estimated, 100 chars each)", 100)
-    .option("-t, --tail [follow]", "Follow the tail of the file")
+    .option("-t, --tail", "Follow the tail of the file")
     .option("-p, --pause [msecs]", "Pause in msecs between pollings", 1000)
-    .action(function(url, opts){
-        n = opts.number;
-        t = opts.tail;
-        p = opts.pause;
+    .option("-v, --verbose", "Use #number instead of url in output")
+    .action(function(url){
+        n = parseInt(this.number);
+        t = typeof this.tail != "undefined";
+        v = typeof this.verbose != "undefined";
+        p = this.pause;
 
         console.log("url: %s", url);
-        console.log("lines: %s", opts.number);
-        console.log("follow: %s", opts.tail);
+        console.log("lines: %s", n);
+        console.log("follow: %s", t);
 
-        console.log("starting httptail on %s", url)
-        options.url = url;
-        request.head(options, function(err, httpResponse, body){
-            var start = parseInt(httpResponse.headers["content-length"]) - (opts.number * 100);
-            go(start);
-        });
+        for (i in url) {
+            var tailer = new HttpTailer(url[i], i, n, p, t, v);
+            tailer.start();
+        }
     })
     .parse(process.argv);
 
 if (!program.args.length)
     program.outputHelp();
-
-    
-function go(start) {
-    var thisChunkSize;
-    var stop = false;
-    options.headers = {
-        "Range": "bytes=" + (start) + "-"
-    }
-    request
-        .get(options)
-        .on('response', function(response) {
-            if (response.statusCode != 206) {
-            //if (response.statusCode == 416) {
-                stop = true;
-                this.emit("end");
-
-
-                setTimeout(function(){
-                    go(start);
-                }, 2000)
-
-                return;
-            }
-            thisChunkSize = parseInt(response.headers['content-length']);
-        })
-        .pipe(byline.createStream())
-        .on('data', function(chunk){
-            //console.log("line %d: %s", n++, chunk);
-            console.log(chunk.toString());
-        })
-        .on('end', function(chunk){
-            if (!stop && t) {
-                setTimeout(function(){
-                    go(start + thisChunkSize);
-                }, p)
-            }
-        })
-}
 
 function parserExample() {
     // https://github.com/sematext/logagent-js/blob/master/patterns.yml
